5f525ce1ebf03b5de4281c19665768791990ef7e
Gustavo <gustavo.cunha@lincros.com>
2022-09-06 16:32:23 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,9 @@
+import Logo from '../../assets/notification-icon.svg';
+
+export function NotificationButton() {
+  return (
+    <div className="dsmeta-red-btn">
+        <img src={Logo} alt="Notificar"/>
+    </div>
+  )
+}
